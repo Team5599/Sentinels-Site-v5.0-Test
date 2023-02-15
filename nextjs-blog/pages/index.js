498785e2fed9ff3fac52e15b4f9687ce88975a15
joeulam <joeulam345@gmail.com>
2023-02-14 23:11:07 -0500

    Barebone

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -3,7 +3,14 @@ import styles from '../styles/Home.module.css'; //replace with css file
 export default function Home() {
   return (
     <div className={styles.container}>
+      <div class="navbar"></div>                {/*sticky navbar*/}
+
+      <div class="block"></div>                 {/*TBD what content to put maybe a photo?*/}
+
       <h1>Cardozo's Robotics team</h1>
+
+
+      <footer></footer>                          {/*Footer information*/}
     </div>
   )
 }
