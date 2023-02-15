6f6756f490984a22c21e248dfa4844c3da3ff066
joeulam <joeulam345@gmail.com>
2023-02-14 22:44:01 -0500

    Setup

---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Home.module.css'; //replace with css file
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>Cardozo's Robotics team</h1>
+    </div>
+  )
+}
