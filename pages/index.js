be5069045bcea4f4a7a123fa1461047082f22d1c
Sentinels Programming <sentinels.pro@gmail.com>
2023-04-03 15:55:46 -0400

    mr p update

--- pages/index.js
+++ pages/index.js
@@ -8,13 +8,11 @@ export default function Home() {
     <>
       <Head>
         <title>Sentinels</title>
-        
       </Head>
       <main>
           <nav class="navbar">
-            <li id="home"><a>The Sentinels</a></li>
+            <li id="home"><a href='home.js'>The Sentinels</a></li>
             <li id="list"><a>Team</a></li>
-            
             <li id="list"><a>Robots</a></li>
             <li id="list"><a>Blogs</a></li>
             <li id="list"><a>Sponsors</a></li>
@@ -22,9 +20,18 @@ export default function Home() {
             <li id="list"><a>Contact us</a></li>
           </nav>
         
-        <div>
+        <body>
+          <div class="img">
+            <Image
+              src="/_DSC2042.JPG"
+              width={1600}
+              height={300}
+            />
+          </div>
+
+
           <h1 class="Heading">News heading</h1>
-        </div>
+        </body>
       </main>
     </>
   )
